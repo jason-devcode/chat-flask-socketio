d2ef3a9eef9f419bf41e5ea73ec5291dcf00e85b
jason-devcode <80931913+jason-devcode@users.noreply.github.com>
2025-03-13 23:27:24 -0500

    feat: add tailwind and path aliases

--- frontend/real-time-chat/vite.config.ts
+++ frontend/real-time-chat/vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+
+      // Assets alias paths
+      "@chatIcons": path.resolve(__dirname, "src/assets/icons"),
+      "@chatStyles": path.resolve(__dirname, "src/assets/styles"),
+
+      // UI alias paths
+      "@chatApp": path.resolve(__dirname, "src/ui/app"),
+      "@chatWidgets": path.resolve(__dirname, "src/widgets"),
+      "@chatLayouts": path.resolve(__dirname, "src/layouts"),
+      "@chatPages": path.resolve(__dirname, "src/pages"),
+
+      // Core alias paths
+      "@chatCore": path.resolve(__dirname, "src/core"),
+      "@chatInterfaces": path.resolve(__dirname, "src/core/interfaces"),
+      "@chatTypes": path.resolve(__dirname, "src/core/types"),
+      "@chatUtils": path.resolve(__dirname, "src/core/utils"),
+      "@chatApi": path.resolve(__dirname, "src/core/api"),
+    },
+  },
+});

1cc34dd62227d6cf9bfb7ee1befa59d9bf580349
jason-devcode <80931913+jason-devcode@users.noreply.github.com>
2025-03-14 23:41:28 -0500

    feat: add login page component

--- frontend/real-time-chat/src/ui/app/App.tsx
+++ frontend/real-time-chat/src/ui/app/App.tsx
@@ -1,5 +1,10 @@
 import "@chatStyles/App.css";
+import { Login } from "@chatPages/login/components/Login";
 
 export const App = () => {
-  return <div className="flex w-screen h-screen"></div>;
+  return (
+    <div className="flex w-screen h-screen">
+      <Login />
+    </div>
+  );
 };
---
+++ frontend/real-time-chat/src/ui/pages/login/components/Login.tsx
@@ -0,0 +1,5 @@
+import { Viewport } from "@/ui/widgets/Viewport/components/Viewport";
+
+export const Login = () => {
+  return <Viewport className="bg-white"></Viewport>;
+};

b980252c9ca94ebb1e2e2cf5189d5615de0878d6
jason-devcode <80931913+jason-devcode@users.noreply.github.com>
2025-03-14 23:31:53 -0500

    feat: add viewport widget

---
+++ frontend/real-time-chat/src/ui/widgets/Viewport/components/Viewport.tsx
@@ -0,0 +1,40 @@
+import { useCallback } from "react";
+
+/**
+ * A flexible `Viewport` component that renders a `div` element and optionally centers its content.
+ * It supports all standard HTML `div` attributes and an additional `fullyCentered` prop.
+ *
+ * @component
+ * @param {Object} props - The component props.
+ * @param {boolean} [props.fullyCentered=false] - If `true`, centers the content both horizontally and vertically using Flexbox.
+ * @param {React.HTMLAttributes<HTMLDivElement>} props - Standard HTML div attributes.
+ * @returns {JSX.Element} The rendered `Viewport` component.
+ */
+export const Viewport = (
+  props: React.HTMLAttributes<HTMLDivElement> & { fullyCentered?: boolean }
+) => {
+  /**
+   * Builds the style object dynamically based on the `fullyCentered` prop.
+   *
+   * @returns {React.CSSProperties} The computed styles.
+   */
+  const buildStyles = useCallback(
+    () => ({
+      ...(props.fullyCentered
+        ? {
+            justifyContent: "center",
+            alignItems: "center",
+          }
+        : {}),
+    }),
+    [props.fullyCentered]
+  );
+
+  return (
+    <div
+      {...props}
+      className={props.className}
+      style={{ ...props.style, ...buildStyles() }}
+    ></div>
+  );
+};

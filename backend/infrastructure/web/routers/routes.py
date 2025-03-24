c48a3519b9bed5cff56ed6bcf9303785a2f13011
jason-devcode <80931913+jason-devcode@users.noreply.github.com>
2025-03-24 18:43:23 -0500

    feat: add roles, protect and description field

--- backend/infrastructure/web/routers/routes.py
+++ backend/infrastructure/web/routers/routes.py
@@ -1,10 +1,10 @@
 from common.constants.http_methods import HttpEndpointMethods
+from common.constants.roles import RoleBitPosition
 from infrastructure.web.controllers.user import UserController
 
 user_controller = UserController()
 
 API_ENDPOINT_PREFIX = "/api/v1"
-
 
 ROUTES = [
     # User endpoints
@@ -12,5 +12,8 @@ ROUTES = [
         "path": f"{API_ENDPOINT_PREFIX}/user/read-user",
         "method": HttpEndpointMethods.GET.value,
         "handler": user_controller.read_user,
+        "roles": [RoleBitPosition.USER],
+        "protect": True,  # by default this is True
+        "description": "",
     },
 ]

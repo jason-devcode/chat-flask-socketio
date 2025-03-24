408a6d930ae2499c13a196e0c36f45426b83c10a
jason-devcode <80931913+jason-devcode@users.noreply.github.com>
2025-03-23 23:49:34 -0500

    refactor: apply pre-commit refactors

--- backend/domain/repositories/user.py
+++ backend/domain/repositories/user.py
@@ -7,14 +7,14 @@ class UserRepository(ABC):
     def __init__(self):
         pass
 
-    def create(self, user_data: User) -> bool | None:
+    def create(self, user: User) -> bool | None:
         raise Exception("Create user method is not implemented")
 
-    def get_by_id(self, uuid: str) -> User | None:
+    def get_by_id(self, id: str) -> User | None:
         raise Exception("get user by id is not implemented")
 
-    def delete_by_id(self, uuid: str) -> bool | None:
+    def delete_by_id(self, id: str) -> bool | None:
         raise Exception("Delete user method is not implemented")
 
-    def update(self, uuid: str, user_data: User) -> bool | None:
+    def update(self, id: str, user: User) -> bool | None:
         raise Exception("Update user method is not implemented")
